aac19e66f014ccbc1dfb449995f5305e79227c5c
TboneDevs <43727067+TboneDevs@users.noreply.github.com>
2018-09-30 12:20:51 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * Vortex
+ * Version 1.2.0
+ * Tanner Durst
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
